c072936207bc68c13ece5acdd8471dc23c061e63
Elizaveta Petrova <lisadreamer@Elizavetas-MacBook-Pro.local>
2018-07-11 01:59:03 +0900

    fetch posts

--- client/components/PostList.js
+++ client/components/PostList.js
@@ -3,11 +3,21 @@ import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 
 class PostList extends Component {
+
+    renderPosts() {
+        return this.props.data.posts.map(post => {
+            return (
+                <li>
+                    {post.title}
+                </li>
+            ) 
+        })
+    }
     render() {
-        
+        if (this.props.data.loading) { return <div>Loading</div>}
         return (
             <div>
-                Posts 
+                {this.renderPosts()} 
             </div>
         );
     }
