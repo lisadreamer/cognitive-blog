2013c254801a542c3cdddc4f8d74ebd793a0eb13
Elizaveta Petrova <lisadreamer@Elizavetas-MacBook-Pro.local>
2018-07-12 00:02:23 +0900

    added delete post buttons

--- client/components/PostList.js
+++ client/components/PostList.js
@@ -5,16 +5,25 @@ import { Link } from 'react-router';
 import fetchPosts from '../queries/fetchPosts';
 
 class PostList extends Component {
+    onPostDelete(id) {
+        this.props.mutate({
+            variables: {
+                id
+            }
+        }).then(() => this.props.data.refetch());
+    }
 
     renderPosts() {
         return this.props.data.posts.map(post => {
             return (
                 <li key={post.id} className="collection-item">
                     {post.title}
+                    <i className="material-icons" onClick={() => this.onPostDelete(post.id)}>delete</i>
                 </li>
             ) 
         })
     }
+
     render() {
         if (this.props.data.loading) { return <div>Loading</div>}
         return (
@@ -33,4 +42,14 @@ class PostList extends Component {
     }
 }
 
-export default graphql(fetchPosts)(PostList);
+const mutation = gql`
+    mutation delPost($id: ID) {
+        deletePost(id: $id) {
+            id
+        }
+    }
+`;
+
+export default graphql(mutation)(
+    graphql(fetchPosts)(PostList)
+);
